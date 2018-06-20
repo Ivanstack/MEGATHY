a3bb6ac7deb50505ebfb0e5e4f70d1a22fc4daa0
tm-315 <topguru315@gmail.com>
2018-06-20 19:05:24 +0530

    configuration changes

--- android/app/src/main/java/com/megathyrn/MainApplication.java
+++ android/app/src/main/java/com/megathyrn/MainApplication.java
@@ -2,21 +2,26 @@ package com.megathyrn;
 
 import android.app.Application;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.LoggingBehavior;
 import com.facebook.react.ReactApplication;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.reactnativenavigation.NavigationReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactnativenavigation.bridge.NavigationReactPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+
 
   private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
 
@@ -56,6 +61,13 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    FacebookSdk.setApplicationId(getString(R.string.facebook_app_id));
+    //initialize Facebook SDK
+    FacebookSdk.sdkInitialize(getApplicationContext());
+    if (BuildConfig.DEBUG) {
+      FacebookSdk.setIsDebugEnabled(true);
+      FacebookSdk.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
+    }
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
