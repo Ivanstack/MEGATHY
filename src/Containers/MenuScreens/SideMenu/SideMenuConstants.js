ef3209d7962748a8dd50410d987d2a038c1f6006
tm-315 <topguru315@gmail.com>
2018-07-16 18:04:19 +0530

    Added new Images and side menu screen

---
+++ src/Containers/MenuScreens/SideMenu/SideMenuConstants.js
@@ -0,0 +1,77 @@
+// Localization
+import React from "react";
+import { Image, Dimensions, StyleSheet } from "react-native";
+
+// Constant
+import * as constant from "../../../Helper/Constants";
+import * as commonUtilities from "../../../Helper/CommonUtilities";
+
+module.exports = {
+    sideMenuItems: [
+        {
+            name: "Categories",
+            icon: commonUtilities.menuImage(constant.menuItemsKeys.category, false),
+            iconActive: commonUtilities.menuImage(constant.menuItemsKeys.category, true),
+        },
+        {
+            name: "Delivery Details",
+            icon: commonUtilities.menuImage(constant.menuItemsKeys.deliveryDetails, false),
+            iconActive: commonUtilities.menuImage(constant.menuItemsKeys.deliveryDetails, true),
+        },
+        {
+            name: "Order History",
+            icon: commonUtilities.menuImage(constant.menuItemsKeys.orderHistory, false),
+            iconActive: commonUtilities.menuImage(constant.menuItemsKeys.orderHistory, true),
+        },
+        {
+            name: "Scheduled Orders",
+            icon: commonUtilities.menuImage(constant.menuItemsKeys.scheduledOrders, false),
+            iconActive: commonUtilities.menuImage(constant.menuItemsKeys.scheduledOrders, true),
+        },
+        {
+            name: "Order Status",
+            icon: commonUtilities.menuImage(constant.menuItemsKeys.orderStatus, false),
+            iconActive: commonUtilities.menuImage(constant.menuItemsKeys.orderStatus, true),
+        },
+        {
+            name: "User Profile",
+            icon: commonUtilities.menuImage(constant.menuItemsKeys.profile, false),
+            iconActive: commonUtilities.menuImage(constant.menuItemsKeys.profile, true),
+        },
+        {
+            name: "My Rewards Wallet",
+            icon: commonUtilities.menuImage(constant.menuItemsKeys.wallet, false),
+            iconActive: commonUtilities.menuImage(constant.menuItemsKeys.wallet, true),
+        },
+        {
+            name: "Change Store",
+            icon: commonUtilities.menuImage(constant.menuItemsKeys.changeStore, false),
+            iconActive: commonUtilities.menuImage(constant.menuItemsKeys.changeStore, true),
+        },
+        {
+            name: "Suggest a Product",
+            icon: commonUtilities.menuImage(constant.menuItemsKeys.suggestProduct, false),
+            iconActive: commonUtilities.menuImage(constant.menuItemsKeys.suggestProduct, true),
+        },
+        {
+            name: "Terms of Services",
+            icon: commonUtilities.menuImage(constant.menuItemsKeys.termsOfServices, false),
+            iconActive: commonUtilities.menuImage(constant.menuItemsKeys.termsOfServices, true),
+        },
+        {
+            name: "Contact us",
+            icon: commonUtilities.menuImage(constant.menuItemsKeys.contact, false),
+            iconActive: commonUtilities.menuImage(constant.menuItemsKeys.contact, true),
+        },
+        {
+            name: "Chat with us",
+            icon: commonUtilities.menuImage(constant.menuItemsKeys.feedback, false),
+            iconActive: commonUtilities.menuImage(constant.menuItemsKeys.feedback, true),
+        },
+        {
+            name: "Logout",
+            icon: commonUtilities.menuImage(constant.menuItemsKeys.logout, false),
+            iconActive: commonUtilities.menuImage(constant.menuItemsKeys.logout, true),
+        },
+    ],
+};
---
+++ src/Containers/MenuScreens/SideMenu/SideMenuScreen.js
@@ -0,0 +1,125 @@
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { StyleSheet, ScrollView, Text, View, TouchableOpacity, Image } from "react-native";
+import { NavigationActions, DrawerItems } from "react-navigation";
+
+// Constant
+import * as constant from "../../../Helper/Constants";
+import * as sideMenuConstant from "./SideMenuConstants";
+import * as commonUtilities from "../../../Helper/CommonUtilities";
+
+// Localization
+import baseLocal from "../../../Resources/Localization/baseLocalization";
+
+export default class SideMenuScreen extends Component {
+    _onPressLogout = () => {
+        this.props.onPressLogout("abc");
+    };
+
+    render = () => {
+        return (
+            <ScrollView>
+                <View style={styles.maincontainer}>
+                    <View style={styles.headercontainer}>
+                        <Text style={styles.headerTitle}>
+                            {global.currentUser === null ? "Welcome, Guest" : "Welcome, " + global.currentUser.userName}
+                        </Text>
+                    </View>
+
+                    <View style={{ flex: 1, backgroundColor: "black" }}>
+                        <DrawerItems
+                            {...this.props.drawerProps}
+                            onItemPress={({ route, focused }) => {
+                                this.props.drawerProps.onItemPress({ route, focused });
+                                constant.debugLog("item pressed " + route.key);
+                                if (route.key === "AddressListScreen") {
+                                    // this.props.drawerProps.getFirstScreenTap()
+                                } else if (route.key === "SecondScreen") {
+                                    // this.props.drawerProps.getSecondScreenTap()
+                                    // this.props.drawerProps.getSecondScreenTap()
+                                }
+                            }}
+                            getLabel={scene => {
+                                constant.debugLog("Navigation Scene: " + JSON.stringify(scene));
+                                let menuItemDetails = sideMenuConstant.sideMenuItems[scene.index];
+                                return (
+                                    <View style={styles.rowView}>
+                                        {scene.focused ? menuItemDetails.iconActive : menuItemDetails.icon}
+                                        <Text
+                                            style={[
+                                                styles.menuTitle,
+                                                { color: scene.focused ? constant.themeColor : "white" },
+                                            ]}
+                                        >
+                                            {baseLocal.t(menuItemDetails.name)}
+                                        </Text>
+                                    </View>
+                                );
+                            }}
+                        />
+                    </View>
+                    <TouchableOpacity onPress={this._onPressLogout}>
+                        <View style={styles.rowView}>
+                            <Image
+                                style={styles.menuImage}
+                                source={require("../../../Resources/Images/MenuIcons/logout.png")}
+                            />
+                            <Text style={styles.menuTitle}>Logout</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        );
+    };
+}
+
+const styles = StyleSheet.create({
+    maincontainer: {
+        flex: 1,
+    },
+    headercontainer: {
+        flex: 1,
+        height: 120,
+        backgroundColor: "black",
+    },
+    userimage: {
+        height: 60,
+        width: 60,
+        resizeMode: "contain",
+        marginTop: 20,
+        marginLeft: 20,
+        borderWidth: 1,
+        borderRadius: 30,
+    },
+    headerTitle: {
+        fontFamily: constant.themeFont,
+        fontSize: 16,
+        fontWeight: "bold",
+        color: "white",
+        marginLeft: 20,
+        marginTop: 60,
+    },
+    headerSubTitle: {
+        fontSize: 15,
+        color: "white",
+        marginLeft: 20,
+    },
+    rowView: {
+        flexDirection: "row",
+        flex: 1,
+        // justifyContent: 'center',
+        alignItems: "center",
+        padding: 10,
+        // marginTop:10
+    },
+    menuImage: {
+        resizeMode: "contain",
+        marginLeft: 10,
+    },
+    menuTitle: {
+        fontFamily: constant.themeFont,
+        fontSize: 17,
+        marginLeft: 16,
+        color: "white",
+    },
+});
